33c5e3e168c9d7baa328fa3121473d5949a8cb5a
Mahmoud Alfaramawy <101274098+Ma7moud-Emad@users.noreply.github.com>
2026-03-28 23:04:16 +0200

    mockAPI.io
    Updated base URL for API requests and changed moveTask method to use PUT instead of PATCH.

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 import type { ColumnKey, Task } from "../types";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  // baseURL: "http://localhost:4000",
+    baseURL: "https://69c8401963393440b31783ff.mockapi.io",
+
 });
 
 export const getTasks = async (): Promise<Task[]> => {
@@ -17,7 +19,7 @@ export const updateTask = (id: string, task: Partial<Task>): Promise<Task> =>
   api.put(`/tasks/${id}`, task).then((r) => r.data);
 
 export const moveTask = (id: string, column: ColumnKey): Promise<Task> =>
-  api.patch(`/tasks/${id}`, { column }).then((r) => r.data);
+  api.put(`/tasks/${id}`, { column }).then((r) => r.data);
 
 export const deleteTask = (id: string): Promise<void> =>
   api.delete(`/tasks/${id}`);
